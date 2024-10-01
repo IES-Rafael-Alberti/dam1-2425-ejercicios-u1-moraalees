9b0980d362a5b90d1b95d4d4a35ca2ac3ef40f72
cmorcan602 <mccristian@iesrafaelalberti.es>
2024-10-01 19:34:46 +0200

    otro mas

--- src/ej03.py
+++ src/ej03.py
@@ -3,5 +3,8 @@ alto = 12.0
 a = ancho / 2
 b = ancho // 2
 c = alto / 2
-d = a + b * 5
-print(a, b, c, d)
+d = 1 + 2 * 5
+print(f"ancho / 2 = {a}")
+print(f"ancho // 2 = {b}")
+print(f"alto / 2 = {c}")
+print(f"1 + 2 * 5 = {d}")
---
+++ src/ej13.py
@@ -0,0 +1,5 @@
+n = int(input("Introduzca un número entero: "))
+m = int(input("Introduzca otro número entero: "))
+c = n/m
+r = n%m
+print(f"La división de {n} entre {m} da un cociente {c} y un resto {r}")
