36d526b5c761d1edf26feda46506d6785800736d
cmorcan602 <mccristian@iesrafaelalberti.es>
2024-10-17 20:07:49 +0200

    Corrección 2.0

--- src/ej20.py
+++ src/ej20.py
@@ -2,7 +2,7 @@ def main():
     num = input("Escriba su número de teléfono junto a su prefijo (+34-) y su extensión (-xx): ")
     num = num.split("-")[1]
     
-    print(num)
+    print(f"Su número de tfno. sin extensiones ni prefijos es {num}")
     
     
 if __name__ == "__main__":
